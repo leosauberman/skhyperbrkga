6a9e767dd50ac66c3e7346e4020a83d1abcaba44
Leonardo Sauberman <leosauberman@gmail.com>
2022-10-14 18:25:05 -0300

    UP: Implementa parametros booleanos; NEW: Comparação entre GridSearch e HyperBRKGA

--- adaptee.py
+++ adaptee.py
@@ -18,8 +18,8 @@ from sklearn.base import is_classifier
 from sklearn.datasets import make_classification
 from sklearn.metrics import check_scoring
 from sklearn.metrics._scorer import _check_multimetric_scoring
-from sklearn.model_selection import check_cv
-from sklearn.model_selection._search import BaseSearchCV, ParameterGrid
+from sklearn.model_selection import check_cv, train_test_split
+from sklearn.model_selection._search import BaseSearchCV, ParameterGrid, GridSearchCV
 from sklearn.model_selection._validation import _fit_and_score, _warn_or_raise_about_fit_failures, _insert_error_scores
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import indexable
@@ -51,6 +51,8 @@ class Decoder:
                 hyperparameters[key] = limits[round(gene * (len(limits) - 1))]
             elif type(limits[0]) is int and len(limits) > 2:
                 hyperparameters[key] = int(limits[round(gene * (len(limits) - 1))])
+            elif type(limits[0]) is bool:
+                hyperparameters[key] = 1 if limits[0] else 0
             else:
                 hyperparameters[key] = (gene * (limits[1] - limits[0])) + limits[0]
 
@@ -118,7 +120,7 @@ class HyperBRKGASearchCV(BaseSearchCV):
 
         self.brkga.initialize()
 
-    def fit_new(self, X, y=None, *, groups=None, **fit_params):
+    def fit(self, X, y=None, *, groups=None, **fit_params):
         estimator = self.estimator
         refit_metric = "score"
 
@@ -213,166 +215,6 @@ class HyperBRKGASearchCV(BaseSearchCV):
 
         return self
 
-    def fit(self, X, y=None, *, groups=None, **fit_params):
-        estimator = self.estimator
-        refit_metric = "score"
-
-        if callable(self.scoring):
-            scorers = self.scoring
-        elif self.scoring is None or isinstance(self.scoring, str):
-            scorers = check_scoring(self.estimator, self.scoring)
-        else:
-            scorers = _check_multimetric_scoring(self.estimator, self.scoring)
-            self._check_refit_for_multimetric(scorers)
-            refit_metric = self.refit
-
-        X, y, groups = indexable(X, y, groups)
-        fit_params = _check_fit_params(X, fit_params)
-
-        cv_orig = check_cv(self.cv, y, classifier=is_classifier(estimator))
-        n_splits = cv_orig.get_n_splits(X, y, groups)
-
-        base_estimator = clone(self.estimator)
-
-        parallel = Parallel(n_jobs=self.n_jobs, pre_dispatch=self.pre_dispatch)
-
-        fit_and_score_kwargs = dict(
-            scorer=scorers,
-            fit_params=fit_params,
-            return_train_score=self.return_train_score,
-            return_n_test_samples=True,
-            return_times=True,
-            return_parameters=False,
-            error_score=self.error_score,
-            verbose=self.verbose,
-        )
-        results = {}
-        with parallel:
-            all_candidate_params = []
-            all_out = []
-            all_more_results = defaultdict(list)
-
-            def evaluate_candidates(candidate_params, cv=None, more_results=None):
-                cv = cv or cv_orig
-                candidate_params = list(candidate_params)
-                n_candidates = len(candidate_params)
-
-                if self.verbose > 0:
-                    print(
-                        "Fitting {0} folds for each of {1} candidates,"
-                        " totalling {2} fits".format(
-                            n_splits, n_candidates, n_candidates * n_splits
-                        )
-                    )
-
-                out = parallel(
-                    delayed(_fit_and_score)(
-                        clone(base_estimator),
-                        X,
-                        y,
-                        train=train,
-                        test=test,
-                        parameters=parameters,
-                        split_progress=(split_idx, n_splits),
-                        candidate_progress=(cand_idx, n_candidates),
-                        **fit_and_score_kwargs,
-                    )
-                    for (cand_idx, parameters), (split_idx, (train, test)) in product(
-                        enumerate(candidate_params), enumerate(cv.split(X, y, groups))
-                    )
-                )
-                print('---------------------------------------------')
-                print(out)
-                print('---------------------------------------------\n')
-
-                if len(out) < 1:
-                    raise ValueError(
-                        "No fits were performed. "
-                        "Was the CV iterator empty? "
-                        "Were there no candidates?"
-                    )
-                elif len(out) != n_candidates * n_splits:
-                    raise ValueError(
-                        "cv.split and cv.get_n_splits returned "
-                        "inconsistent results. Expected {} "
-                        "splits, got {}".format(n_splits, len(out) // n_candidates)
-                    )
-
-                _warn_or_raise_about_fit_failures(out, self.error_score)
-
-                # For callable self.scoring, the return type is only know after
-                # calling. If the return type is a dictionary, the error scores
-                # can now be inserted with the correct key. The type checking
-                # of out will be done in `_insert_error_scores`.
-                if callable(self.scoring):
-                    _insert_error_scores(out, self.error_score)
-
-                all_candidate_params.extend(candidate_params)
-                all_out.extend(out)
-
-                if more_results is not None:
-                    for key, value in more_results.items():
-                        all_more_results[key].extend(value)
-
-                nonlocal results
-                results = self._format_results(
-                    all_candidate_params, n_splits, all_out, all_more_results
-                )
-
-                return results
-
-            self._run_search(evaluate_candidates)
-
-            # multimetric is determined here because in the case of a callable
-            # self.scoring the return type is only known after calling
-            first_test_score = all_out[0]["test_scores"]
-            self.multimetric_ = isinstance(first_test_score, dict)
-
-            # check refit_metric now for a callabe scorer that is multimetric
-            if callable(self.scoring) and self.multimetric_:
-                self._check_refit_for_multimetric(first_test_score)
-                refit_metric = self.refit
-
-        # For multi-metric evaluation, store the best_index_, best_params_ and
-        # best_score_ iff refit is one of the scorer names
-        # In single metric evaluation, refit_metric is "score"
-        if self.refit or not self.multimetric_:
-            self.best_index_ = self._select_best_index(
-                self.refit, refit_metric, results
-            )
-            if not callable(self.refit):
-                # With a non-custom callable, we can select the best score
-                # based on the best index
-                self.best_score_ = results[f"mean_test_{refit_metric}"][
-                    self.best_index_
-                ]
-            self.best_params_ = results["params"][self.best_index_]
-
-        if self.refit:
-            # we clone again after setting params in case some
-            # of the params are estimators as well.
-            self.best_estimator_ = clone(
-                clone(base_estimator).set_params(**self.best_params_)
-            )
-            refit_start_time = time.time()
-            if y is not None:
-                self.best_estimator_.fit(X, y, **fit_params)
-            else:
-                self.best_estimator_.fit(X, **fit_params)
-            refit_end_time = time.time()
-            self.refit_time_ = refit_end_time - refit_start_time
-
-            if hasattr(self.best_estimator_, "feature_names_in_"):
-                self.feature_names_in_ = self.best_estimator_.feature_names_in_
-
-        # Store the only scorer not as a dict for single metric evaluation
-        self.scorer_ = scorers
-
-        self.cv_results_ = results
-        self.n_splits_ = n_splits
-
-        return self
-
     def _run_search(self, evaluate_candidates):
         evaluate_candidates(ParameterGrid(self._parameters))
 
@@ -382,34 +224,60 @@ class HyperBRKGASearchCV(BaseSearchCV):
 if __name__ == '__main__':
     iris = datasets.load_iris()
     params = {'C': [1, 10], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']}
-    # Como lidar com casos como esse:
-    # degree: int, default=3
-    # Degree of the polynomial kernel function (‘poly’). Ignored by all other kernels.
 
     svc = svm.SVC()
 
     clf = HyperBRKGASearchCV(svc, parameters=params, data=iris.data, target=iris.target, verbose=3)
 
-    clf.fit_new(iris.data, iris.target)
+    clf.fit(iris.data, iris.target)
     print(clf.cv_results_)
 """
 
-"""
 # Exemplo 2
 if __name__ == '__main__':
     param_grid = {
         'max_depth': [2, 4, 8, 16, 32, 64],
-        'min_samples_leaf': [2, 4, 8, 16]
+        'min_samples_leaf': [2, 4, 8, 16],
+        'criterion': ['gini', 'entropy', 'log_loss']
     }
 
     tree = DecisionTreeClassifier()
+    treeHBRKGA = DecisionTreeClassifier(criterion='entropy', max_depth=64, min_samples_leaf=2)
+    treeGS = DecisionTreeClassifier(criterion='gini', max_depth=4, min_samples_leaf=4)
+    iris = datasets.load_iris()
 
-    np.random.seed(1)
-    X, y = make_classification(n_samples=400, n_features=6, n_informative=6,
-                               n_redundant=0, n_classes=10, class_sep=2)
+    # hyperbrkga = HyperBRKGASearchCV(tree, parameters=param_grid, cv=10, scoring='accuracy',
+    #                                 data=iris.data, target=iris.target, refit=True, verbose=3)
+    # hyperbrkga.fit(iris.data, iris.target)
+    # print('HyperBRKGA')
+    # print(hyperbrkga.cv_results_)
+    # print('--------------------------------------------\n')
+    #
+    grid = GridSearchCV(tree, param_grid)
+    grid.fit(iris.data, iris.target)
+    print('GridSearch')
+    print(grid.best_params_, grid.best_score_)
 
-    hyperbrkga = HyperBRKGASearchCV(tree, parameters=param_grid, cv=10, scoring='accuracy',
-                                    data=X, target=y, refit=True, verbose=3)
-    hyperbrkga.fit_new(X, y)
-    print(hyperbrkga.cv_results_)
+    # X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=1, stratify=iris.target)
+    #
+    # treeHBRKGA.fit(X_train, y_train)
+    # treeGS.fit(X_train, y_train)
+    #
+    # print(treeHBRKGA.score(X_test, y_test))
+    # print(treeGS.score(X_test, y_test))
+
+
+
 """
+HyperBRKGA
+'best_param_decoded': 
+{'criterion': 'entropy', 'max_depth': 64, 'min_samples_leaf': 2},
+'best_param_score': 0.98, 
+'total_time': datetime.timedelta(seconds=1, microseconds=725785)}
+
+Grid Search
+{'criterion': 'gini', 'max_depth': 4, 'min_samples_leaf': 4}
+
+"""
+
+
